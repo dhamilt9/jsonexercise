653c7fdf073ecac652498fa9a9f55ebfa5843396
Dan Hamilton <danhamilton16@gmail.com>
2016-03-09 16:45:25 +0000

    made everything look nice

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 		method: 'GET'
 	}).then(function(data) {
 		$.each(data, function( index, value ){
-			$('#column').append("<div class='post'><div class='clear'></div><div class='title'>" + value.title + "</div><div class='body'>" + value.body + "</div></div>");
+			// $('#column').append("<div class='post'><div class='clear'></div><div class='title'>" + value.title + "</div><div class='body'>" + value.body + "</div></div>");
+			$('#column').append("<div class='post'><div class='clear'>x</div><div class='post_left'><div class='username'>User "+value.userId+"</div><div class='userimage'>User image goes here</div></div><div class='post_right'><div class='title'>" + value.title + "</div><div class='body'>" + value.body + "</div></div></div>");
 		});
 	});
 });
